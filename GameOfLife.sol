d2ffc5f4842988a40b65b1bf0e070a1f3b0bb222
Peter <peter.sedgewick@gmail.com>
2017-12-18 19:06:57 +0000

    Update contract

--- GameOfLife.sol
+++ GameOfLife.sol
@@ -3,8 +3,62 @@ pragma solidity ^0.4.18;
 
 contract GameOfLife {
     uint8 public state;
+    address[] neighbours;
 
-    function ChangeState(uint8 newState) public {
+    // Get neighbours
+    function getNeighbours() public returns (address neighbour) {
+        return neighbours;
+    }
+
+    // Get neighbours count
+    function getNeighbourCount() public constant returns (uint count) {
+        return neighbours.length;
+    }
+
+    // Add new neighbour
+    function addNeighbour(address neighbour) public {
+        if (this.getNeighbourCount() > 7) {
+            return;
+        }
+
+        neighbours.push(address);
+    }
+
+    // Take a step forward
+    function step() public {
+        uint8 aliveNeighbours = 0;
+
+        for (uint i; i < this.getNeighbourCount(); i++) {
+            if (this.neighbours[i].getState() == 0) {
+                aliveNeighbours++;
+            }
+        }
+
+        if (this.getState() == 1) {
+            if (aliveNeighbours < 2) {
+                // I starve.
+                this.setState(0);
+            } else if (aliveNeighbours == (2 | 3)) {
+                // I survive.
+            } else if (aliveNeighbours > 3) {
+                // I erupt.
+                this.setState(0);
+            }
+        } else {
+            if (aliveNeighbours == 3) {
+                // I emerge.
+                this.setState(1);
+            }
+        }
+    }
+
+    // Set state to argument.
+    function setState(uint8 newState) public {
         state = newState;
+    }
+
+    // Return the current state.
+    function getState() view public returns (uint8) {
+        return state;
     }
 }
