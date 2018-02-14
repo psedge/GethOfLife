0ce1b10304e49732dc4e7e2bf33c26cbd4585708
Peter Sedgewick <peter.sedgewick@gmail.com>
2018-02-14 04:35:14 +0000

    Update init scripts for private net, genesis config for testnet.

--- start.sh
+++ start.sh
@@ -1,1 +1,1 @@
-geth --identity "GoL Private" --rpc --rpcport "8080" --rpccorsdomain "*" --datadir ./testnet --port "30303" --nodiscover --rpcapi "db,eth,net,web3" --networkid 15 console
+geth --identity "GoL Private" --rpc --rpcaddr "0.0.0.0" --rpcport "8080" --rpccorsdomain "*" --datadir ./testnet --port "30303" --nodiscover --rpcapi "db,eth,net,web3" --networkid 15 console
